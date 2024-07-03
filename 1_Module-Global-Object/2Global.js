ed6f55ddc587a7e8e8b0c231e0a1db7b744ef861
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-07-03 14:04:01 +0530

    Add files via upload

---
+++ 1_Module-Global-Object/2Global.js
@@ -0,0 +1,16 @@
+/*
+
+setTimeout(() => {
+    console.log("Hello");
+}, 5000); 
+
+*/
+// this will print "Hello" after 5 seconds/ 5000 milli-seconds
+
+console.log(global);
+// console.log(global); // will give all the global variables and functions
+// main function of global is : 
+// 1. setTimeout()
+// 2. setInterval()
+// 3. clearTimeout()
+// 4. clearInterval()
