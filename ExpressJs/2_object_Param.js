b627142326e8905bdda0682f65b5625c0977a851
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-07-07 17:02:47 +0530

    .

--- ExpressJs/2_object_Param.js
+++ ExpressJs/2_object_Param.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.post("/example" , (request, response, next)=>{
+app.post("/example", (request, response, next)=>{
     let data = request.body;
     console.log(data);
     response.send("This is example response");
