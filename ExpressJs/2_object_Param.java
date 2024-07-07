59b6975ccb690b31a86ffc640db8b6224765ec8e
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-07-07 17:00:45 +0530

    Create 2_object_Param.java

---
+++ ExpressJs/2_object_Param.java
@@ -0,0 +1,13 @@
+const { name } = require("ejs");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.post("/example" , (request, response, next)=>{
+    let data = request.body;
+    console.log(data);
+    response.send("This is example response");
+})
+app.listen(8000 , function(){
+    console.log("Server is running at port 8000");
+})
