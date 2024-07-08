0af36c140ed29b75c6fb4fa6cce703d8e6bafc8b
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-07-08 17:18:31 +0530

    Cookies

---
+++ ExpressJs/5_Cookie.js
@@ -0,0 +1,19 @@
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+
+app.get("/example",(req,res)=>{
+    // const {email} = req.cookies;
+    // console.log(email);
+
+    // res.cookie("name","joe");
+    // res.cookie("age","22");
+
+    res.clearCookie("name");
+    res.clearCookie("age");
+    res.send("example route");
+});
+
+app.listen(8000,()=>{
+    console.log("Server is running at port 8000");
+});
