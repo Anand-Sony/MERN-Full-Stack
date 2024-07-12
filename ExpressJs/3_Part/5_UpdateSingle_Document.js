1a1dca7f7258f9f78cacd2140ae180928451a169
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-07-12 12:46:31 +0530

    update single document using Mongoose (Method 1)

--- ExpressJs/3_Part/5_UpdateSingle_Document.js
+++ ExpressJs/3_Part/5_UpdateSingle_Document.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app= express();
 app.use(express.json());
-
 const connectionUrl = "mongodb://localhost:27017/school_Db";
 mongoose
     .connect(connectionUrl)
