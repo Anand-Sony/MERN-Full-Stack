f758f05e0d1dfe2255fefddfd44e93e5ebfb18db
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-08-12 11:13:26 +0530

    Add files via upload

---
+++ React_Course/src/components/Button.jsx
@@ -0,0 +1,6 @@
+import { memo } from "react";
+const Button = ({handleClick , children})=>{
+    console.log(`Rendering button ${children}`);
+    return <button onClick={handleClick}>{children}</button>
+};
+export default memo(Button);
