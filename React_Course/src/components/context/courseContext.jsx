8fb772dbb53722371bf86821359c207c6b7c377a
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-08-12 11:13:57 +0530

    Add files via upload

---
+++ React_Course/src/components/context/courseContext.jsx
@@ -0,0 +1,4 @@
+// import createContext from "../CustomContext";   you don't need this becuase react provide us createContext method
+import { createContext } from "react";
+const CourseContext = createContext(); // let the value to be null, we are not passing any value
+export default CourseContext;
