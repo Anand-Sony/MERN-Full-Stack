dfb24349b8f60d94ead3e5207d64d6731b04109c
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-08-15 09:50:39 +0530

    Add files via upload

---
+++ React_Course/src/components/ComponentTwo.jsx
@@ -0,0 +1,10 @@
+import ComponentThree from "./ComponentThree";
+const ComponentTwo = ()=>{
+    return(
+        <div>
+            <p>Component Two</p>
+            <ComponentThree />
+        </div>
+    )
+}
+export default ComponentTwo;
