f30749bb4d9dcada92911a982a8e37a41a03c1ca
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-08-15 09:49:33 +0530

    Add files via upload

---
+++ React_Course/src/components/ClickCounterByHOC.jsx
@@ -0,0 +1,6 @@
+import withCounter from "./HOC/withCounter";
+
+const ClickCounterByHOC = ({counter , handleIncrement})=>{
+    return <button onClick={handleIncrement} >Clicked {counter} times </button>
+};
+export default withCounter(ClickCounterByHOC);
