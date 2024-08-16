5ab7ea436da8ae70abf98cce26afff0c7aa1a94c
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-08-16 13:50:29 +0530

    Add files via upload

---
+++ React_Course/src/components/CssStyle.jsx
@@ -0,0 +1,9 @@
+//import "../assets/css/style.css";
+const CssStyle = ()=>{
+    return (
+        <div className="container">
+            <p>Css Styling</p>
+        </div>
+    )
+};
+export default CssStyle;
